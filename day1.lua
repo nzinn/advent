87ca81cd7968672a26da1406e3a07379fb59e43c
Noah Zinn <noahwzinn@gmail.com>
2022-05-16 11:26:18 -0500

    fully implemented utils

--- day1.lua
+++ day1.lua
@@ -4,7 +4,10 @@ local utils = require("utils")
 
 
 
-local file_table = utils.file_to_ntable("./input.txt")
+local file_table = utils.file_to_ntable("input.txt")
+
+
+print(file_table[1] + file_table[2])
 
 
 
--- utils.lua
+++ utils.lua
@@ -1,23 +1,28 @@
 local utils = {}
 
-
-function utils.file_to_ntable(file)
+--takes a file and returns a table of numbers 
+function utils.file_to_ntable(file_name)
 
    local file_table = {}
+
+   local i = 0
    
-   for i, lines in io.lines(file, "*n") do
-      file_table[i] = lines 
+   for line in io.lines(file_name) do
+      file_table[i] = tonumber(line)
+      i = i + 1
    end
 
    return file_table
 end
 
-
+--takes a file and returns a table of lines 
 function utils.file_to_stable(file)
    local file_table = {}
 
-   for i, lines in io.lines(file) do
+   local i = 1
+   for line in io.lines(file) do
       file_table[i] = lines
+      i = i + 1
    end
    
    return file_table
