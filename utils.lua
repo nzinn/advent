426e40ed5f8a7235475832cdf003b2ec204952fe
Noah Zinn <noahwzinn@gmail.com>
2022-05-16 10:47:01 -0500

    finished utils

--- utils.lua
+++ utils.lua
@@ -5,12 +5,19 @@ function file_to_ntable(file)
    for i, lines in io.lines(file, "*n") do
       file_table[i] = lines 
    end
+
+   return file_table
 end
 
 
 function file_to_stable(file)
+   local file_table = {}
 
-
+   for i, lines in io.lines(file) do
+      file_table[i] = lines
+   end
+   
+   return file_table
 end
 
 return {file_to_ntable, file_to_stable}
